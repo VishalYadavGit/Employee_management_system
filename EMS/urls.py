ca398cee37b2a398773b75fd6c80b8ff95e24784
XEGENDvy <vishalsyadav4@gmail.com>
2023-11-20 00:42:44 +0530

    trying to server media file

--- EMS/urls.py
+++ EMS/urls.py
@@ -39,3 +39,4 @@ if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
         
 urlpatterns += staticfiles_urlpatterns()
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
